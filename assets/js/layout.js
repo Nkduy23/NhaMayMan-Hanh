98034d34c4b908cee635f262a12c24bfe04fa2f6
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2025-11-10 02:10:47 +0700

    update base url support github page

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,10 +1,19 @@
-fetch("layouts/header.html")
+if (!window.BASE_PATH) {
+  // Fallback nếu config chưa load
+  const isDev =
+    window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost";
+  window.BASE_PATH = isDev ? "/" : "/NhaMayMan-Hanh/";
+}
+
+const baseUrl = window.location.origin + window.BASE_PATH;
+
+fetch(`${baseUrl}layouts/header.html`)
   .then((res) => res.text())
   .then((data) => {
     document.querySelector("header").innerHTML = data;
   });
 
-fetch("layouts/footer.html")
+fetch(`${baseUrl}layouts/footer.html`)
   .then((res) => res.text())
   .then((data) => {
     document.querySelector("footer").innerHTML = data;
