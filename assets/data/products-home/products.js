ca8022258c51b7710cb72ff82d8cc207ad276a0d
nguyenduc050514 <nguyenvanduc050514@gmail.com>
2025-11-09 22:05:58 +0700

    banner-home

---
+++ assets/data/products-home/products.js
@@ -0,0 +1,13 @@
+const products = [
+   {
+      product_name: "Con chuột",
+      price_range: 3.3,
+      description:
+         "Sản phẩm do các bạn khuyết tật kém may mắn sản xuất thủ công kính mong nhận được sự ủng hộ của Quý khách hàng.",
+      material: "Vải thổ cẩm",
+      product_code: "TC01TB",
+      hotline: "0909 06 25 28",
+      vietnam_contact_info:
+         "Nếu bạn ở Việt Nam vui lòng liên hệ hệ thống chúng tôi theo số hotline hoặc fanpage doanh nghiệp Nhà May Mắn chúng tôi để biết giá bán tại Việt Nam.",
+   },
+];
