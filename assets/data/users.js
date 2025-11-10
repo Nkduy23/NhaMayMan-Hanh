451133beec534175ece6426006eb92a28f385877
Bao Tran <saichien862410@gmail.com>
2025-11-10 13:25:33 +0700

    update login, register, forgot password, trạng thái hiển thị user trên header

---
+++ assets/data/users.js
@@ -0,0 +1,38 @@
+// data/users.js
+export const users = [
+  {
+    id: 1,
+    name: "Nguyễn Văn A",
+    phone: "0912345678",
+    email: "a.nguyen@example.com",
+    password: "Passw0rd!",
+  },
+  {
+    id: 2,
+    name: "Trần Thị B",
+    phone: "0987654321",
+    email: "b.tran@example.com",
+    password: "Welcome123",
+  },
+  {
+    id: 3,
+    name: "Lê Văn C",
+    phone: "0901111222",
+    email: "c.le@example.com",
+    password: "MySecret1",
+  },
+  {
+    id: 4,
+    name: "Phạm Thị D",
+    phone: "0977000111",
+    email: "d.pham@example.com",
+    password: "Admin@2024",
+  },
+  {
+    id: 5,
+    name: "Hoàng Văn E",
+    phone: "0933222110",
+    email: "e.hoang@example.com",
+    password: "Qwerty!23",
+  },
+];
--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,11 +1,35 @@
-fetch("layouts/header.html")
+fetch("../layouts/header.html")
   .then((res) => res.text())
   .then((data) => {
     document.querySelector("header").innerHTML = data;
+    updateHeaderUser();
   });
-
-fetch("layouts/footer.html")
+fetch("../layouts/footer.html")
   .then((res) => res.text())
   .then((data) => {
     document.querySelector("footer").innerHTML = data;
   });
+
+// Cập nhật header dựa trên trạng thái đăng nhập
+function updateHeaderUser() {
+  const userMenu = document.querySelector("#user-menu");
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  if (currentUser) {
+    // Nếu đã đăng nhập
+    userMenu.innerHTML = `
+      <span>Xin chào, <b style="color: red">${currentUser.name}</b></span> | 
+      <a href="#" id="logout-link" >Đăng xuất</a>
+    `;
+
+    // Xử lý khi bấm đăng xuất
+    document
+      .getElementById("logout-link")
+      .addEventListener("click", function (e) {
+        e.preventDefault();
+        localStorage.removeItem("currentUser");
+        alert("Đăng xuất thành công!");
+        location.reload(); // Tải lại trang để cập nhật giao diện
+      });
+  }
+}
